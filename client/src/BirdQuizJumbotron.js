188883ceeaf34e28e10fdb8826669298b3112526
Mattias Henriksson <henriksson.mj@gmail.com>
2018-02-03 11:39:36 +0100

    Move to Quiz tab on Start new quiz button click

--- client/src/BirdQuizJumbotron.js
+++ client/src/BirdQuizJumbotron.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
+import { Route } from 'react-router-dom';
 
 class BirdQuizJumbotron extends Component {
 
@@ -9,7 +10,14 @@ class BirdQuizJumbotron extends Component {
               	<h1>Fågelquiz</h1>
               	<p>Välkommen till Fågelquiz! Arta fåglar och få poäng.</p>
               	<p>
-                	<Button bsStyle="primary" onClick={() => this.props.startNewQuiz()}>Starta quiz</Button>
+                	{/*<Button bsStyle="primary" onClick={() => this.props.startNewQuiz()}>Starta quiz</Button>*/}
+                  <Route 
+                    render={({history}) => (
+                      <Button bsStyle="primary" onClick={() => {history.push('/quiz')} }>
+                        Starta Quiz
+                      </Button>
+                    )} 
+                  />
               	</p>
             </Jumbotron>
         );
