1cc77de435c6b1006d4db9821d296b251a8306e5
Mattias Henriksson <henriksson.mj@gmail.com>
2018-02-04 15:23:07 +0100

    Adding start quiz button on Quiz-page

--- client/src/Quiz.js
+++ client/src/Quiz.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
-import {Image} from 'react-bootstrap';
+import {Image, Button} from 'react-bootstrap';
 import ResponseForm from './ResponseForm';
 
 class Quiz extends Component {
+	constructor() {
+		super();
+		this.state = {
+			isStartButtonHidden: false
+		}
+
+		this.toggleStartButtonHidden = this.toggleStartButtonHidden.bind(this);
+	}
+
+	toggleStartButtonHidden() {
+		this.setState({
+			isStartButtonHidden: !this.state.isStartButtonHidden
+		});
+	}
 
 	render() {
 		return (
 			<div>
+				<div className="row">
+					<div className="col">
+						{!this.state.isStartButtonHidden && 
+							<Button bsStyle="primary" bsSize="large" onClick={this.toggleStartButtonHidden}>
+								Starta Quiz!
+							</Button>
+						}
+					</div>
+				</div>
+
 		        <div className="row">
 		          <div className="col-sm-6">
 		            <h1>{this.props.formatTime(this.props.time)}</h1>
